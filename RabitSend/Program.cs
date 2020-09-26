1a0153ebd6d62f6310d650638455748c6f48376f
Piotr <piotrekkaskow@gmail.com>
2020-09-26 10:19:00 +0200

    Add project files.

---
+++ RabitSend/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabitSend
+{
+    internal class Program
+    {
+        private class Send
+
+        {
+            public static void Main()
+            {
+                var factory = new ConnectionFactory() { HostName = "10.10.1.4", UserName = "oneprod", Password = "c5mvmZN2kqOrVKVYFRs_XX2sVfZBVUun" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "test-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+                    string message = "dickens";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "", routingKey: "test-queue", basicProperties: null, body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
